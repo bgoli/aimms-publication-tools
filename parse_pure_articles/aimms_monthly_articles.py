cf42dac844bdacab3a3b377908124d656ec5fb43
Brett Olivier <brettolivier@gmail.com>
2020-08-10 13:20:37 +0200

    imported from systemsbioinfromatics

---
+++ parse_pure_articles/aimms_monthly_articles.py
@@ -0,0 +1,165 @@
+import os
+import time
+import pprint
+import re
+import json
+import CBNetDB
+import xlrd
+
+ctime = time.strftime('%Y-%m-%d')
+cdir = os.path.dirname(os.path.abspath(os.sys.argv[0]))
+data_dir = os.path.join(cdir, 'data')
+data_file = 'AIMMS_research_2020-7_08_20.xls'
+
+# using excel file directly
+data = {}
+Bk = xlrd.open_workbook(os.path.join(data_dir, data_file))
+St = Bk.sheet_by_index(0)
+month = St.col(0)
+paper = St.col(1)
+for r in range(len(paper)):
+    try:
+        if str(int(month[r].value)) in data:
+            data[str(int(month[r].value))].append(str(paper[r].value))
+        else:
+            data[str(int(month[r].value))] = [str(paper[r].value)]
+    except:
+        pass
+
+## using exported tsv file
+#data_file = 'AIMMS_research_2020-3_08_20.txt'
+#data = {}
+#with open(os.path.join(data_dir, data_file), 'r') as F:
+    ##csvread = csv.reader(F, dialect="excel-tab")
+    #for r in F:
+        #l = r.split('\t')
+        #if len(l) < 2:
+            #continue
+        #if l[0] in data:
+            #data[l[0]].append(l[1])
+        #else:
+            #data[l[0]] = [l[1]]
+#pprint.pprint(data)
+
+re_pub_data = re.compile(r"Publication date:(.*?)Handle")
+re_early_date =  re.compile(r"Early online date:(.*?)Publication")
+re_contrib  = re.compile(r"Contributors:(.*?)(?:Number|Publication)")
+re_corresp  = re.compile(r"Corresponding author:(.*?)Contributors")
+re_journal  = re.compile(r"Journal:(.*?)Volume")
+re_doi      = re.compile(r"DOIs:(.*?)URLs")
+re_orgs     = re.compile(r"Organisations:(.*?)(?:Contributors|Corresponding)")
+re_title    = re.compile(r"^(.*?[a-z\?])[A-Z]")
+re_descript = re.compile(r"^(.*?)General information")
+
+parsed_data = {}
+parsed_data_nodoi = {}
+data_keys = []
+for m in [str(a+1) for a in range(int(time.strftime('%m')))]:
+    cntr = 1
+    for p in data[m]:
+        NODOI = False
+        pclean = p.replace('\xa0', '')
+        pub_date_match = re_pub_data.search(pclean)
+        early_date_match = re_early_date.search(pclean)
+        contrib_match = re_contrib.search(pclean)
+        corresp_match = re_corresp.search(pclean)
+        journal_match = re_journal.search(pclean)
+        doi_match = re_doi.search(pclean)
+        orgs_match = re_orgs.search(pclean)
+        title_match = re_title.search(pclean)
+        descript_match = re_descript.search(pclean)
+
+        print('Processing record: {} ...'.format(cntr))
+        if doi_match is not None:
+            #print(m, cntr, 'https://doi.org/'+doi_match.groups()[0])
+            key = 'https://doi.org/'+doi_match.groups()[0]
+        else:
+            key = str(time.monotonic()).split('.')[0]
+            NODOI = True
+
+        parsed_data[key] = {}
+        parsed_data[key]['month'] = m
+
+        if pub_date_match is not None:
+            #print(m, cntr, pub_date_match.groups()[0])
+            parsed_data[key]['pub_date'] = pub_date_match.groups()[0]
+            parsed_data[key]['year'] = int(parsed_data[key]['pub_date'].split(' ')[-1])
+        else:
+            parsed_data[key]['year'] = time.strftime('%Y')
+        if early_date_match is not None:
+            #print(m, cntr, title_match.groups()[0])
+            parsed_data[key]['early_online'] = early_date_match.groups()[0]
+        else:
+            parsed_data[key]['early_online'] = None
+        if title_match is not None:
+            #print(m, cntr, title_match.groups()[0])
+            parsed_data[key]['title'] = title_match.groups()[0]
+        else:
+            parsed_data[key]['title'] = None
+        if contrib_match is not None:
+            #print(m, cntr, contrib_match.groups()[0])
+            parsed_data[key]['contributors'] = contrib_match.groups()[0]
+        else:
+            parsed_data[key]['contributors'] = None
+        if corresp_match is not None:
+            #print(m, cntr, corresp_match.groups()[0])
+            parsed_data[key]['corresponding'] = corresp_match.groups()[0]
+        else:
+            parsed_data[key]['corresponding'] = None
+        if orgs_match is not None:
+            #print(m, cntr, orgs_match.groups()[0])
+            parsed_data[key]['organisations'] = orgs_match.groups()[0]
+        else:
+            parsed_data[key]['organisations'] = None
+        if journal_match is not None:
+            #print(m, cntr, journal_match.groups()[0])
+            parsed_data[key]['journal'] = journal_match.groups()[0]
+        else:
+            parsed_data[key]['journal'] = None
+        if descript_match is not None:
+            #print(m, cntr, descript_match.groups()[0])
+            parsed_data[key]['description'] = descript_match.groups()[0]
+        else:
+            parsed_data[key]['description'] = None
+        data_keys = tuple(parsed_data[key].keys())
+        cntr += 1
+        if NODOI:
+            parsed_data_nodoi[key] = parsed_data.pop(key)
+
+pprint.pprint(parsed_data)
+pprint.pprint(parsed_data_nodoi)
+for k in parsed_data:
+    print(k)
+
+with open(os.path.join(data_dir, 'current_doi_dataset.json'), 'w') as F:
+    json.dump(parsed_data, F, indent=1)
+with open(os.path.join(data_dir, 'current_nodoi_dataset.json'), 'w') as F:
+    json.dump(parsed_data_nodoi, F, indent=1)
+
+# setup/connect/load database
+table_cols = ['doi TEXT PRIMARY KEY', 'newdata TEXT']
+_ = [table_cols.append('{} TEXT'.format(a)) for a in data_keys]
+print(table_cols)
+
+aimmsDB = CBNetDB.DBTools()
+aimmsDB.connectSQLiteDB('aimmsDB.sqlite', work_dir=cdir)
+test = aimmsDB.getTable('publications')
+if aimmsDB.getTable('publications') is None:
+    aimmsDB.createDBTable('publications', table_cols)
+table_pub = aimmsDB.getTable('publications')
+
+new_data = []
+for doi in parsed_data:
+    if len(aimmsDB.getRow('publications', 'doi', doi)) == 0:
+        dta = parsed_data[doi].copy()
+        dta['doi'] = doi
+        dta['newdata'] = 'True'
+        aimmsDB.insertData('publications', dta, commit=False)
+        new_data.append(doi)
+        print('New data inserted for DOI: {}'.format(doi))
+    else:
+        aimmsDB.updateData('publications', 'doi', doi, {'newdata' : 'False'}, commit=False)
+aimmsDB.commitDB()
+aimmsDB.closeDB()
+
+
---
+++ parse_pure_articles/generate_publication_report.py
@@ -0,0 +1,144 @@
+import os
+import time
+
+# import pprint
+import CBNetDB
+import docx
+import json
+
+# import numpy
+
+DO_ALL = False
+
+ctime = time.strftime('%Y-%m-%d')
+cdir = os.path.dirname(os.path.abspath(os.sys.argv[0]))
+data_dir = os.path.join(cdir, 'data')
+
+sqlite_file = 'aimmsDB.sqlite'
+json_file = os.path.join(data_dir, 'current_doi_dataset.json')
+
+with open(json_file, 'r') as F:
+    parsed_data = json.load(F)
+
+aimmsDB = CBNetDB.DBTools()
+aimmsDB.connectSQLiteDB('aimmsDB.sqlite', work_dir=cdir)
+
+sldata = aimmsDB.getColumns('publications', ['doi', 'month', 'newdata'])
+
+new_papers = []
+month_papers = []
+print(len(sldata[0]))
+for d in range(len(sldata[0])):
+    print(int(sldata[1][d]), int(time.strftime('%m')), eval(sldata[2][d]))
+    if (
+        int(sldata[1][d]) in range(1, int(time.strftime('%m')) + 1)
+        and eval(sldata[2][d])
+    ) or DO_ALL:
+        new_papers.append(aimmsDB.getRow('publications', 'doi', sldata[0][d])[0])
+    elif int(sldata[1][d]) in (int(time.strftime('%m')), int(time.strftime('%m')) - 1):
+        month_papers.append(aimmsDB.getRow('publications', 'doi', sldata[0][d])[0])
+
+del sldata
+
+# new_papers = []
+# darray = numpy.array(sldata)
+# darray[[1,0,2], :] = darray.copy()
+# darray = darray.transpose()
+# darray.sort()
+# darray= darray.transpose()
+# print(darray)
+# print(darray.shape)
+
+# print(darray[0])
+# print(darray[1])
+# print(darray[2])
+# for d in range(darray.shape[1]):
+# if (int(darray[0][d]) == int(time.strftime('%m')) and bool(darray[1][d])) or DO_ALL:
+# new_papers.append(aimmsDB.getRow('publications', 'doi', darray[2][d])[0])
+# del darray
+
+aimmsDB.closeDB()
+
+# pprint.pprint(new_papers)
+
+Dx = docx.Document()
+Dx2news = docx.Document()
+
+_ = Dx.add_heading('AIMMS publication report for: {}'.format(ctime), level=1)
+_ = Dx2news.add_heading('AIMMS publication report for: {}'.format(ctime), level=1)
+
+cntr = 0
+curr_month = None
+for d in new_papers:
+    # print(d)
+    p0 = Dx.add_paragraph(
+        '{} ({}-{})'.format(d[6][:60], d[4], d[2]), style='List Number'
+    )
+    if cntr == 0:
+        curr_month = int(d[2])
+        cntr += 1
+
+for d in month_papers:
+    # print(d)
+    p0 = Dx.add_paragraph(style='List Number')
+    p0.add_run('{} ({}-{})'.format(d[6][:60], d[4], d[2])).italic = True
+
+
+def add_detail_list(doc, D):
+    x = doc.add_paragraph(D[7], style='List Bullet')
+    x = doc.add_paragraph(D[9], style='List Bullet')
+    x = doc.add_paragraph(D[10], style='List Bullet')
+    x = doc.add_paragraph(D[0], style='List Bullet')
+    x = doc.add_paragraph('Corresponding author: {}'.format(D[8]), style='List Bullet')
+    x = doc.add_paragraph(
+        'Published {} (early online {})'.format(D[3], D[5]), style='List Bullet'
+    )
+    x = doc.add_paragraph('Processed: {}-{}'.format(D[4], D[2]), style='List Bullet')
+    del x
+
+
+def add_newsletter_item(doc, D):
+    p = doc.add_paragraph()
+    p.add_run(D[7] + ' ')
+    p.add_run(D[6] + ' ').bold = True
+    p.add_run('({}, {})[{}]'.format(D[10], D[3], D[0]))
+
+
+_ = Dx2news.add_heading(level=3).add_run(
+    'New papers: {}-{}'.format(time.strftime('%Y'), time.strftime('%m'))
+)
+
+for d in new_papers:
+    # detailed doc
+    h = Dx.add_heading(level=3)
+    h.add_run('{}) {}'.format(cntr, d[6]))
+    add_detail_list(Dx, d)
+    if DO_ALL:
+        p = Dx.add_paragraph(d[11].replace(d[6], '')[:301] + ' ...')
+    else:
+        p = Dx.add_paragraph(d[11].replace(d[6], ''))
+    p.add_run(' ')
+
+    # newsletter
+    add_newsletter_item(Dx2news, d)
+
+    cntr += 1
+
+_ = Dx2news.add_heading(level=3).add_run('New papers: {}'.format(time.strftime('%Y')))
+
+for d in month_papers:
+    # detailed doc
+    h = Dx.add_heading(level=3)
+    h.add_run('{}) {}'.format(cntr, d[6])).italic = True
+    add_detail_list(Dx, d)
+    p = Dx.add_paragraph(d[11].replace(d[6], '')[:200] + ' ...')
+    p.add_run(' ')
+
+    # newsletter
+    add_newsletter_item(Dx2news, d)
+
+    cntr += 1
+
+Dx.save('AIMMS_publication_report-{}.docx'.format(ctime))
+Dx2news.save('AIMMS_publications_for_newsletter-{}.docx'.format(ctime))
+time.sleep(2)
