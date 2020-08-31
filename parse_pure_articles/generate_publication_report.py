50add83c4ed8377d9606a816a9bb29a01cc42361
Brett Olivier <brettolivier@gmail.com>
2020-08-31 23:13:36 +0200

    playing with selection code

--- parse_pure_articles/generate_publication_report.py
+++ parse_pure_articles/generate_publication_report.py
@@ -29,13 +29,16 @@ new_papers = []
 month_papers = []
 print(len(sldata[0]))
 for d in range(len(sldata[0])):
-    print(int(sldata[1][d]), int(time.strftime('%m')), eval(sldata[2][d]))
+    print(sldata[0][d], sldata[1][d], sldata[2][d])
     if (
         int(sldata[1][d]) in range(1, int(time.strftime('%m')) + 1)
         and eval(sldata[2][d])
     ) or DO_ALL:
         new_papers.append(aimmsDB.getRow('publications', 'doi', sldata[0][d])[0])
-    elif int(sldata[1][d]) in (int(time.strftime('%m')), int(time.strftime('%m')) - 1):
+    if int(sldata[1][d]) in (
+        int(time.strftime('%m')),
+        # int(time.strftime('%m')) - 1,
+    ) and eval(sldata[2][d]):
         month_papers.append(aimmsDB.getRow('publications', 'doi', sldata[0][d])[0])
 
 del sldata
