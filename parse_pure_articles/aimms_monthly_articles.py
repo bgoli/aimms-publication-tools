fed2e3b382ebf694f7b75c9624839d56b666f16c
bgoli <brettolivier@gmail.com>
2021-09-13 10:55:45 +0200

    september data 1

--- parse_pure_articles/aimms_monthly_articles.py
+++ parse_pure_articles/aimms_monthly_articles.py
@@ -79,7 +79,8 @@ __version__ = 1.5
 # data_file = 'AIMMS_research_2021-22_03_21.xls'
 # data_file = 'AIMMS_research_2021-31_05_21.xls'
 # data_file = 'AIMMS_research_2021-14_06_21.xls'
-data_file = 'AIMMS_research_2021-28_06_21.xls'
+# data_file = 'AIMMS_research_2021-28_06_21.xls'
+data_file = 'AIMMS_research_2021-13_09_21.xls'
 
 import os
 import time
